01e37718d75122fbd640f4d1b66bfcdbd5bbf29e
Frankline <franklinen.ononiwu@gmail.com>
2021-04-24 18:30:02 -0400

    code upgrade

--- run.sh
+++ run.sh
@@ -1,19 +1,3 @@
 #!/bin/sh
-import pandas as pd
-url = 'https://gist.githubusercontent.com/bobbae/b4eec5b5cb0263e7e3e63a6806d045f2/raw/279b794a834a62dc108fc843a72c94c49361b501/data.csv'
-data = pd.read_csv(url)
-# Total rows
-print(data.shape[0])
-#rename columns
-data.columns = ['year', 'company', 'rank', 'revenue', 'profit']
-# Convert to float, otherwise NaN
-data['profit'] = pd.to_numeric(data['profit'], errors='coerce')
-# Remove NaN
-data = data[data['profit'].notnull()]
-# Rows after removing NaN
-print(data.shape[0])
-# Sort by descending order of profit
-data.sort_values(by=['profit'], ascending=False, inplace=True)
-# Export top 20 to .json
-data[:20].to_json('data2.json', orient='records')
+python test.py
 
--- test.py
+++ test.py
@@ -1,4 +1,12 @@
+#declare environment variables
+import os
+os.chdir(r'C:\Users\MAIN\Desktop\sada')
+os.getcwd()
+#import libraries
+import json
 import pandas as pd
+
+#import file
 url = 'https://gist.githubusercontent.com/bobbae/b4eec5b5cb0263e7e3e63a6806d045f2/raw/279b794a834a62dc108fc843a72c94c49361b501/data.csv'
 data = pd.read_csv(url)
 # Total rows
@@ -15,3 +23,11 @@ print(data.shape[0])
 data.sort_values(by=['profit'], ascending=False, inplace=True)
 # Export top 20 to .json
 data[:20].to_json('data2.json', orient='records')
+#import the json file and print it
+with open(r'C:\Users\MAIN\Desktop\sada\data2.json') as f:
+    data2 = json.load(f)
+print(data2)
+
+
+
+
